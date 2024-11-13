467091683b512434bb343932409d423a7888a747
beom <bdh3659@naver.com>
2024-11-14 00:57:08 +0900

    feat: 홈화면 파일 추가

---
+++ vite-project/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+const HomePage = () => {
+  return <Wrapper>Hello World!</Wrapper>
+}
+
+export default HomePage
+
+const Wrapper = styled.div`
+  color: red;
+`
