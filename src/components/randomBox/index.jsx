300bf21ef78a77a04765936b5ac390aacac52878
beom <bdh3659@naver.com>
2024-11-15 22:59:03 +0900

    feat: randombox 컴포넌트 생성

--- src/components/randomBox/index.jsx
+++ src/components/randomBox/index.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import styled from "@emotion/styled"
+
+const randomItem = [
+  {
+    
+  }
+]
 
 const RandomBox = () => {
-  return <div>index</div>
+  return <Wrapper></Wrapper>
 }
 
 export default RandomBox
+
+
